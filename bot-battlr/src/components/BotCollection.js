ce6cf98fadf45b1c8210ed149c00d90453899287
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-07-30 07:58:28 +0300

    added loading in fetch

--- bot-battlr/src/components/BotCollection.js
+++ bot-battlr/src/components/BotCollection.js
@@ -2,13 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 const BotCollection = ({ onEnlist, enlistedBots }) => {
     const [bots, setBots] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [selectedBot, setSelectedBot] = useState(null);
   
     useEffect(() => {
       fetch('http://localhost:8001/bots')
         .then((response) => response.json())
-        .then((data) => setBots(data))
-        .catch((error) => console.error('Error fetching data:', error));
+        .then((data) => {
+          setBots(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          setIsLoading(false);
+        });
     }, []);
   
 }
