3219ad83a02a6a0369e430c46b368276d86cc5d0
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-07-30 07:53:36 +0300

    added error handling in fetch

--- bot-battlr/src/components/BotCollection.js
+++ bot-battlr/src/components/BotCollection.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 const BotCollection = ({ onEnlist, enlistedBots }) => {
-  const [bots, setBots] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8001/bots')
-      .then((response) => response.json())
-      .then((data) => setBots(data));
-  }, []);
+    const [bots, setBots] = useState([]);
+    const [selectedBot, setSelectedBot] = useState(null);
+  
+    useEffect(() => {
+      fetch('http://localhost:8001/bots')
+        .then((response) => response.json())
+        .then((data) => setBots(data))
+        .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+  
 }
-
 export default BotCollection;
