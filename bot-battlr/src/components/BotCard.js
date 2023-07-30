af91dede55a475faf89ee419cff3caa49c51d92d
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-07-30 08:31:08 +0300

    added info handling in botcard

--- bot-battlr/src/components/BotCard.js
+++ bot-battlr/src/components/BotCard.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
 
-const BotCard = () =>{
+const BotCard = () => {
+  const { id, name, health, damage, armor, bot_class, avatar_url, catchphrase } = bot;
 
-}
+  return (
+    <div >
+      <img src={avatar_url} alt={name} />
+      <h3>{name}</h3>
+      <p>Health: {health}</p>
+      <p>Damage: {damage}</p>
+      <p>Armor: {armor}</p>
+      <p>Class: {bot_class}</p>
+      <p>Catchphrase: {catchphrase}</p> 
+     </div>
+  );
+};
 
 export default BotCard;
